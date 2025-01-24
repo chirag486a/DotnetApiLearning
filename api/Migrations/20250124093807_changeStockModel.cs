7e97527e86f7882cae7687b0fe61e092736332dd
chirag <70309029+chirag486a@users.noreply.github.com>
2025-01-24 15:26:11 +0545

    Add MarketCap Datatype Change Migrations

---
+++ api/Migrations/20250124093807_changeStockModel.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace api.Migrations
+{
+    /// <inheritdoc />
+    public partial class changeStockModel : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<decimal>(
+                name: "MarketCap",
+                table: "Stocks",
+                type: "decimal(28,2)",
+                nullable: false,
+                oldClrType: typeof(long),
+                oldType: "bigint");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<long>(
+                name: "MarketCap",
+                table: "Stocks",
+                type: "bigint",
+                nullable: false,
+                oldClrType: typeof(decimal),
+                oldType: "decimal(28,2)");
+        }
+    }
+}
