d6ddcacb2f898bb8f3a0b80884b4e5889a8f3b48
chirag <70309029+chirag486a@users.noreply.github.com>
2025-01-28 15:49:16 +0545

    Add Data Anotation to Dtos

--- api/Dtos/Stock/UpdateStockRequestDto.cs
+++ api/Dtos/Stock/UpdateStockRequestDto.cs
@@ -1,15 +1,30 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace api.Dtos.Stock;
 
 public class UpdateStockRequestDto
 {
+
+  [Required]
+  [MaxLength(10, ErrorMessage = "Symbol cannot be over 10 characters")]
   public string Symbol { get; set; } = string.Empty;
+  [Required]
+  [MaxLength(255, ErrorMessage = "Company name cannot be over 255 names")]
   public string CompanyName { get; set; } = string.Empty;
 
+  [Required]
+  [Range(1, 100000000000)]
   public decimal Price { get; set; }
 
+  [Required]
+  [Range(0.00001, 100)]
   public decimal LastDiv { get; set; }
 
+  [Required]
+  [MaxLength(50, ErrorMessage = "Industry cannot be over 10 characters")]
   public string Industry { get; set; } = string.Empty;
+  [Required]
+  [Range(1, 1000000000000)]
   public decimal MarketCap { get; set; }
 
 }
