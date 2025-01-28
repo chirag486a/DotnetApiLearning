077ebff8de35e1bbcddbb156c68f51c593b3276b
chirag <70309029+chirag486a@users.noreply.github.com>
2025-01-28 15:54:55 +0545

    Update Migration from change model

---
+++ api/Migrations/20250128100920_changeStockModelPropertyName.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace api.Migrations
+{
+    /// <inheritdoc />
+    public partial class changeStockModelPropertyName : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Price",
+                table: "Stocks",
+                newName: "Purchase");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Purchase",
+                table: "Stocks",
+                newName: "Price");
+        }
+    }
+}
